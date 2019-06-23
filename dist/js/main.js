77244eb9bf867ae6dbe85d93de526f76e2325de6
Michael <mejevin@gmail.com>
2019-06-23 07:08:06 +0300

    Changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,43 +1,56 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
 
-const navItems = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll(".nav-item");
 
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu()
-{
-    if (!showMenu)
-    {
-        menuBtn.classList.add('close');
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
 
-        menuNav.classList.add('show');
-        menu.classList.add('show');
-        menuBranding.classList.add('show');
+    menuNav.classList.add("show");
+    menu.classList.add("show");
+    menuBranding.classList.add("show");
 
-        navItems.forEach(item => {
-            item.classList.add('show');
-        });
+    navItems.forEach(item => {
+      item.classList.add("show");
+    });
 
-        showMenu = true;
-    }
-    else
-    {
-        menuBtn.classList.remove('close');
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
 
-        menuNav.classList.remove('show');
-        menu.classList.remove('show');
-        menuBranding.classList.remove('show');
+    menuNav.classList.remove("show");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
 
-        navItems.forEach(item => {
-            item.classList.remove('show');
-        });
-        
-        showMenu = false
-    }
+    navItems.forEach(item => {
+      item.classList.remove("show");
+    });
+
+    showMenu = false;
+  }
+}
+
+const hoverOverElement = document.querySelector("#hover-for-bg-item");
+const mainElement = document.querySelector("#about");
+const overlayElement = document.querySelector(".overlay");
+
+hoverOverElement.addEventListener("mouseover", hideAboutBG);
+hoverOverElement.addEventListener("mouseleave", showAboutBG);
+
+function showAboutBG() {
+  mainElement.classList.remove("hide");
+  overlayElement.classList.remove("hide");
+}
+
+function hideAboutBG() {
+  mainElement.classList.add("hide");
+  overlayElement.classList.add("hide");
 }
