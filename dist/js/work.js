727bad91f43d57ffd1be86309faef53739afd4f1
Michael <mejevin@gmail.com>
2019-09-11 21:21:32 +0300

    Proper sizing of expandables

--- dist/js/work.js
+++ dist/js/work.js
@@ -1,5 +1,9 @@
 const expandableButtons = document.getElementsByClassName("project-group-expand-btn");
 
+
+window.addEventListener("resize", OnResize);
+
+
 for (let i = 0; i < expandableButtons.length; ++i) {
     expandableButtons[i].addEventListener("click", 
     event => {
@@ -10,9 +14,46 @@ for (let i = 0; i < expandableButtons.length; ++i) {
             content.style.maxHeight = "0";
             content.classList.remove("show");
         } else {
-            content.style.maxHeight = document.defaultView.getComputedStyle(content, "").getPropertyValue("grid-template-columns").split(" ")[0];
+            let rowValues =  document.defaultView.getComputedStyle(content, "").getPropertyValue("grid-template-rows").split(" ");
+            let finalContentHeight = expandableButtons[i].clientHeight;
+
+            for (let j = 0; j < rowValues.length; ++j)
+            {
+                finalContentHeight += parseInt(rowValues[j].replace("px", ""));
+            }
+
+            console.log("COmputed height: " + finalContentHeight);
+
+            content.style.maxHeight = finalContentHeight + "px";
             content.classList.add("show");
         }
     }
     );
+}
+
+function OnResize() {
+    for (let i = 0; i < expandableButtons.length; ++i)
+    {
+        console.log(i);
+        var content = expandableButtons[i].nextElementSibling;
+        if (content.classList.contains("show"))
+        {
+            let rowValues =  document.defaultView.getComputedStyle(content, "").getPropertyValue("grid-template-rows").split(" ");
+            let finalContentHeight = expandableButtons[i].clientHeight;
+
+            for (let j = 0; j < rowValues.length; ++j)
+            {
+                finalContentHeight += parseInt(rowValues[j].replace("px", ""));
+            }
+
+            if (content.style.maxHeight == finalContentHeight)
+            {
+                return;
+            }
+
+            console.log("Recomputed: " + content.style.maxHeight + " to " + finalContentHeight);
+
+            content.style.maxHeight = finalContentHeight + "px";
+        }
+    }
 }
