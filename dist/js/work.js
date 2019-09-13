cc96c013c8ebf0bfeb6f0186e960f854f5ba0b9b
Michael <mejevin@gmail.com>
2019-09-14 01:38:03 +0300

    Added projedural addition of project items via JS

--- dist/js/work.js
+++ dist/js/work.js
@@ -1,3 +1,25 @@
+let Categories = [];
+
+// Parent element for all groups of project, i.e. 'C#' 'Delphi' 'C++' etc (categories)
+let ProjectsGroupDiv = document.getElementById("project-group");
+
+AddProject(
+  "Delphi",
+  "Test Name",
+  ["Delphi", "Nigga", "Fuck", "Whatever"],
+  "https://github.com/meJevin/meJevin.github.io",
+  "This is a nigga that just likes to fuck my ass",
+  "img/projects/delphi/ScreenCropper.mp4"
+);
+AddProject(
+  "Delphi",
+  "Test Name",
+  ["Delphi", "Nigga", "Fuck", "Whatever"],
+  "https://github.com/meJevin/meJevin.github.io",
+  "This is a nigga that just likes to fuck my ass",
+  "img/projects/delphi/ScreenCropper.mp4"
+);
+
 const expandableButtons = document.getElementsByClassName(
   "project-group-expand-btn"
 );
@@ -16,12 +38,12 @@ for (let i = 0; i < expandableButtons.length; ++i) {
         .getComputedStyle(content, "")
         .getPropertyValue("grid-template-rows")
         .split(" ");
-      let finalContentHeight = expandableButtons[i].clientHeight;
+      let finalContentHeight = 0;
 
       for (let j = 0; j < rowValues.length; ++j) {
         finalContentHeight += parseInt(rowValues[j].replace("px", ""));
       }
-      
+
       content.style.maxHeight = finalContentHeight + "px";
       content.classList.add("show");
     }
@@ -65,7 +87,9 @@ for (let i = 0; i < infoButtons.length; ++i) {
   });
 }
 
-const projectVideoPreviews = document.getElementsByClassName("item-video-preview");
+const projectVideoPreviews = document.getElementsByClassName(
+  "item-video-preview"
+);
 
 for (let i = 0; i < projectVideoPreviews.length; ++i) {
   projectVideoPreviews[i].addEventListener("mouseenter", event => {
@@ -76,4 +100,131 @@ for (let i = 0; i < projectVideoPreviews.length; ++i) {
     projectVideoPreviews[i].pause();
     projectVideoPreviews[i].currentTime = 0;
   });
+}
+
+function GetCategory(categoryName) {
+  for (let i = 0; i < Categories.length; ++i) {
+    if (
+      categoryName ==
+      Categories[i].getElementsByClassName("project-group-expand-btn")[0]
+        .children[0].innerText
+    ) {
+      return Categories[i];
+    }
+  }
+
+  return null;
+}
+
+function AddProject(
+  categoryName,
+  projectName,
+  tags,
+  projectLink,
+  description,
+  videoPath
+) {
+  let projectGroupElement = document.getElementsByClassName("project-group")[0];
+
+  let category = GetCategory(categoryName);
+
+  if (category == null) {
+    category = AddCategory(categoryName);
+    projectGroupElement.appendChild(category);
+  }
+  console.log(category);
+
+  let categoryContent = category.getElementsByClassName(
+    "project-group-content"
+  )[0];
+
+  let itemElement = document.createElement("div");
+  itemElement.className = "item";
+
+  let itemVideoDiv = document.createElement("div");
+
+  let videoElement = document.createElement("video");
+  videoElement.className = "item-video-preview";
+  videoElement.toggleAttribute("loop");
+  videoElement.toggleAttribute("muted");
+
+  let videoSourceElement = document.createElement("source");
+  videoSourceElement.setAttribute("src", videoPath);
+  videoSourceElement.setAttribute("type", "video/mp4");
+
+  videoElement.appendChild(videoSourceElement);
+
+  itemVideoDiv.appendChild(videoElement);
+
+  itemElement.appendChild(itemVideoDiv);
+
+  let itemDescriptionDiv = document.createElement("div");
+  itemDescriptionDiv.classList = "item-description";
+
+  let itemDescriptionHeaderElement = document.createElement("h3");
+  itemDescriptionHeaderElement.innerHTML = description;
+
+  itemDescriptionDiv.appendChild(itemDescriptionHeaderElement);
+
+  itemElement.appendChild(itemDescriptionDiv);
+
+  let infoIconElement = document.createElement("div");
+  infoIconElement.className = "info-icon";
+
+  let infoIconIElement = document.createElement("i");
+  infoIconIElement.className = "fas fa-info";
+
+  infoIconElement.appendChild(infoIconIElement);
+
+  itemElement.appendChild(infoIconElement);
+
+  let AGitLinkElement = document.createElement("a");
+  AGitLinkElement.setAttribute("href", projectLink);
+  AGitLinkElement.setAttribute("target", "_blank");
+  AGitLinkElement.className = "btn-dark";
+
+  let AGitLinkIconElement = document.createElement("i");
+  AGitLinkIconElement.className = "fab fa-github";
+  AGitLinkIconElement.innerHTML = " " + projectName;
+
+  AGitLinkElement.appendChild(AGitLinkIconElement);
+
+  let tagContainerDivElement = document.createElement("div");
+  tagContainerDivElement.className = "tag-container";
+
+  let tagDivElements = [];
+  for (let i = 0; i < tags.length; ++i) {
+    let itemTagDivElement = document.createElement("div");
+    itemTagDivElement.className = "item-tag";
+
+    let itemHeaderElement = document.createElement("h3");
+    itemHeaderElement.innerHTML = tags[i];
+
+    itemTagDivElement.appendChild(itemHeaderElement);
+    tagContainerDivElement.appendChild(itemTagDivElement);
+  }
+
+  itemElement.appendChild(tagContainerDivElement);
+
+  categoryContent.appendChild(itemElement);
+}
+
+function AddCategory(categoryName) {
+  let categoryHtmlElement = document.createElement("div");
+
+  let projectButton = document.createElement("button");
+  projectButton.className = "project-group-expand-btn";
+  projectButton.appendChild(document.createElement("h3"));
+  projectButton.children[0].innerHTML = categoryName;
+
+  categoryHtmlElement.appendChild(projectButton);
+
+  let projectGroupContent = document.createElement("div");
+  projectGroupContent.className = "project-group-content";
+
+  categoryHtmlElement.appendChild(projectGroupContent);
+
+  Categories.push(categoryHtmlElement);
+
+  return Categories[Categories.length - 1];
 }
